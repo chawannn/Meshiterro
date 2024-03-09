d84a7233806425a979d8979794db37107224caf7
chawannn <158204549+chawannn@users.noreply.github.com>
2024-03-09 05:23:37 +0000

    deviseの設定変更　2024/03/09

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image)
   end
 
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+
 end
